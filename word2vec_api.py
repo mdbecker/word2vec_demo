4d51fad87581281cd98d97a091346018d9784411
Michael Becker <michaelb@aweber.com>
2014-12-10 23:22:12 -0500

    api: Update to use gensim format

--- word2vec_api.py
+++ word2vec_api.py
@@ -4,8 +4,7 @@ from gensim.models.word2vec import Word2Vec
 import json
 
 print 'loading model'
-MODEL = Word2Vec.load_word2vec_format(
-    'GoogleNews-vectors-negative300.bin.gz', binary=True)
+MODEL = Word2Vec.load('GoogleNews-vectors-negative300.gensim')
 print 'model loaded'
 
 app = Flask(__name__)
@@ -16,14 +15,17 @@ class HelloWorld(Resource):
         # reqparse didn't work, when a single item was passed in the negative
         # field It was splitting the string by character
         args = request.get_json()
-        result = MODEL.most_similar(
-            positive=args['positive'],
-            negative=args['negative'],
-            topn=args['topn'],
-        )
+        try:
+            result = MODEL.most_similar(
+                positive=args['positive'],
+                negative=args['negative'],
+                topn=args['topn'],
+            )
+        except KeyError:
+            return {'result': [("Sorry, I haven't learned that word yet", -1)]}, 201
         return {'result': result}, 201
 
 api.add_resource(HelloWorld, '/most_similar')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
